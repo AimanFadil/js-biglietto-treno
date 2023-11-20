3136258c8447d55e01b891f306fad8a6ee524a3e
AimanFadil <aimanbello2001@gmail.com>
2023-11-20 16:21:21 +0100

    push finale

--- js/script.js
+++ js/script.js
@@ -4,19 +4,17 @@ let eta = prompt('Quanti anni hai?')
 
 let totale = kilometri * 0.21;
 
-if (eta < 17) {
-    totale = totale * 20 / 100;
+if (eta <= 17) {
+    totale -= totale * 20 / 100;
 } 
 else if (eta >= 65) {
-    totale = totale * 40 / 100;
+    totale -= totale * 40 / 100;
 } 
 else {
     
 }
-/* console.log(totale) */
-
 
 let n = totale.toFixed(2);
-document.getElementById(`price`).innerHTML = n;
+document.getElementById(`price`).innerHTML = totale;
 console.log(n)
 
